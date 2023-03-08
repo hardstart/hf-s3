c0cf6a6d447931b5a106972ecae7d4fa5c101064
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-03-08 11:50:20 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "arn" {
-  value       = aws_s3_bucket.bucket.arn
+  value       = module.s3.arn
   description = "The s3 bucket arn"
 }
 output "bucket_name" {
-  value       = aws_s3_bucket.bucket.bucket
+  value       = module.s3.bucket_name
   description = "The s3 bucket name"
 }
