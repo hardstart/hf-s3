7fb08a063c5191bb457e4a0b39ab50b6b609f810
Jason Mueller <jmueller@healthfirst.org>
2023-03-08 11:38:07 -0500

    Initial add

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {}
+
+module "s3" {
+  source  = "app.terraform.io/healthfirst/hf-s3/aws"
+  version = "1.0.0"
+  bucket  = var.bucket
+}
